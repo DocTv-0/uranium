34ec6ca7492be9199614641ed6c141ea67ceed65
DocTv-0 <tomasz.m.fiedler@gmail.com>
2026-08-22 10:59:19 +0200

    Add encode_varint

--- main.rs
+++ main.rs
@@ -8,3 +8,18 @@ pub async fn send_packet(stream: &mut TcpStream, packet: Vec<u8>) -> Result<()> 
     stream.flush().await?;
     Ok(())
 }
+
+/// Saves a VarInt to a vector of bytes
+pub fn encode_varint(buffer: &mut Vec<u8>, mut value: i32) {
+    loop {
+        let mut temporary = (value & 0x7F) as u8;
+        value = ((value as u32) >> 7) as i32;
+        if value != 0 {
+            temporary |= 0x80;
+        }
+        buffer.push(temporary);
+        if value == 0 {
+            break;
+        }
+    }
+}
